7f66ae067601b2284fc31871930c34686d25300b
DanialBillows <danialbillows@icloud.com>
2025-09-19 14:29:57 +0200

    develop branch added

--- Grocery.App/ViewModels/GroceryListItemsViewModel.cs
+++ Grocery.App/ViewModels/GroceryListItemsViewModel.cs
@@ -3,7 +3,10 @@ using CommunityToolkit.Mvvm.Input;
 using Grocery.App.Views;
 using Grocery.Core.Interfaces.Services;
 using Grocery.Core.Models;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Grocery.App.ViewModels
 {
@@ -34,6 +37,17 @@ namespace Grocery.App.ViewModels
 
         private void GetAvailableProducts()
         {
+            AvailableProducts.Clear();
+            var producten = _productService.GetAll();
+            foreach (var item in producten)
+            {
+                bool listCheck = MyGroceryListItems.Any(item => item.Id == item.Id);
+                if (listCheck = 0 & producten.Stock > 0)
+                {
+                    AvailableProducts.Add(item);
+                }
+            }
+
             //Maak de lijst AvailableProducts leeg
             //Haal de lijst met producten op
             //Controleer of het product al op de boodschappenlijst staat, zo niet zet het in de AvailableProducts lijst
@@ -54,6 +68,14 @@ namespace Grocery.App.ViewModels
         [RelayCommand]
         public void AddProduct(Product product)
         {
+            var ProductCheck = _productService.Get(product.Id);
+            if (ProductCheck != null & ProductCheck.Stock > 0)
+            {
+                var newProduct = new GroceryListItem(0, groceryList.ID, 1, product.Id);
+                ProductCheck.Stock--;
+                _productService.Update(newProduct);
+                GetAvailableProducts();
+            }
             //Controleer of het product bestaat en dat de Id > 0
             //Maak een GroceryListItem met Id 0 en vul de juiste productid en grocerylistid
             //Voeg het GroceryListItem toe aan de dataset middels de _groceryListItemsService
