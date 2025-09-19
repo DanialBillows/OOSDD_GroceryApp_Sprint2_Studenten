01e122e2215c967d31c6b27ba9da436d713168b1
DanialBillows <danialbillows@icloud.com>
2025-09-19 15:19:02 +0200

    UC6 implemented

--- Grocery.Core.Data/Repositories/ClientRepository.cs
+++ Grocery.Core.Data/Repositories/ClientRepository.cs
@@ -1,5 +1,4 @@
-﻿
-using Grocery.Core.Interfaces.Repositories;
+﻿using Grocery.Core.Interfaces.Repositories;
 using Grocery.Core.Models;
 
 namespace Grocery.Core.Data.Repositories
@@ -19,12 +18,12 @@ namespace Grocery.Core.Data.Repositories
 
         public Client? Get(string email)
         {
-            return clientList[0];
+            return clientList.FirstOrDefault(c => c.EmailAddress == email);
         }
 
         public Client? Get(int id)
         {
-            return clientList[0];
+            return clientList.FirstOrDefault(c => c.Id == id);
         }
 
         public List<Client> GetAll()
