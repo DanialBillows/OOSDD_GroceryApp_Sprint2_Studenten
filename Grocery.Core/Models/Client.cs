a2104e46e1d41c856fb4b625a292a8e39bbe125e
DanialBillows <danialbillows@icloud.com>
2025-09-19 15:22:19 +0200

    email and password public again

--- Grocery.Core/Models/Client.cs
+++ Grocery.Core/Models/Client.cs
@@ -1,14 +1,14 @@
-﻿
-namespace Grocery.Core.Models
+﻿namespace Grocery.Core.Models
 {
     public partial class Client : Model
     {
-        public string _emailAddress; { get; set; }
-        public string _password { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+
         public Client(int id, string name, string emailAddress, string password) : base(id, name)
         {
-            _emailAddress=emailAddress;
-            _password=password;
+            EmailAddress = emailAddress;
+            Password = password;
         }
     }
 }
